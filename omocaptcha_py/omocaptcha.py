b282214f4da5f0c51d9eeca1b10fc133ec184b56
MehdiRtal <mehdirtal7@pm.me>
2023-11-29 21:19:01 +0100

    Refactor OMOCaptcha class to use create_job and get_result methods

--- omocaptcha_py/omocaptcha.py
+++ omocaptcha_py/omocaptcha.py
@@ -7,52 +7,46 @@ class OMOCaptcha:
     def __init__(self, api_key: str):
         self.api_key = api_key
 
-    def solve_tiktok_2objects(self, image_url: str, width: int, height: int):
-        image_content = httpx.get(image_url).content
+    def create_job(self, service_id: int, **data):
         r = httpx.post("https://omocaptcha.com/api/createJob", json={
             "api_token": self.api_key,
             "data": {
-                "type_job_id": 22,
-                "image_base64": base64.b64encode(image_content).decode("utf-8"),
-                "width_view": width,
-                "height_view": height,
+                "type_job_id": service_id,
+                **data
             }
         })
         if r.json()["error"]:
             raise Exception(r.json()["message"])
-        job_id = r.json()["job_id"]
+        return r.json()["job_id"]
+
+    def get_result(self, job_id: int):
         while True:
             r = httpx.post("https://omocaptcha.com/api/getJobResult", json={
             "api_token": self.api_key,
             "job_id": job_id
             })
             if r.json()["status"] == "waiting":
-                time.sleep(1)
+                time.sleep(3)
             elif r.json()["status"] == "fail":
                 raise Exception()
             elif r.json()["status"] == "success":
-                result = r.json()["result"]
-                return int(result.split("|")[0]), int(result.split("|")[1]), int(result.split("|")[2]), int(result.split("|")[3])
+                return r.json()["result"]
+
+    def solve_tiktok_2objects(self, image_url: str):
+        image_content = httpx.get(image_url).content
+        job_id = self.create_job(22, image_base64=base64.b64encode(image_content).decode("utf-8"), width_view=340, height_view=212)
+        result = self.get_result(job_id)
+        return float(result.split("|")[0]), float(result.split("|")[1]), float(result.split("|")[2]), float(result.split("|")[3])
 
     def solve_tiktok_rotation(self, outer_image_url: str, inner_image_url: str):
-        r = httpx.post("https://omocaptcha.com/api/createJob", json={
-            "api_token": self.api_key,
-            "data": {
-                "type_job_id": 23,
-                "image_base64": f"{base64.b64encode(httpx.get(inner_image_url).content).decode('utf-8')}|{base64.b64encode(httpx.get(outer_image_url).content).decode('utf-8')}",
-            }
-        })
-        if r.json()["error"]:
-            raise Exception(r.json()["message"])
-        job_id = r.json()["job_id"]
-        while True:
-            r = httpx.post("https://omocaptcha.com/api/getJobResult", json={
-            "api_token": self.api_key,
-            "job_id": job_id
-            })
-            if r.json()["status"] == "waiting":
-                time.sleep(1)
-            elif r.json()["status"] == "fail":
-                raise Exception()
-            elif r.json()["status"] == "success":
-                return int(r.json()["result"])
+        outer_image_content = httpx.get(outer_image_url).content
+        inner_image_content = httpx.get(inner_image_url).content
+        job_id = self.create_job(23, image_base64=f"{base64.b64encode(inner_image_content).decode('utf-8')}|{base64.b64encode(outer_image_content).decode('utf-8')}")
+        result = self.get_result(job_id)
+        return int(result)
+
+    def solve_tiktok_drag_drop(self, image_url: str):
+        image_content = httpx.get(image_url).content
+        job_id = self.create_job(24, image_base64=base64.b64encode(image_content).decode("utf-8"), width_view=340)
+        result = self.get_result(job_id)
+        return int(result)
